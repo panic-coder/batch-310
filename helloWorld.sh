350b5647c741b89035d6f95fd772ecce492f1198
panic-coder <androidshubham@gmail.com>
2021-06-23 09:19:03 +0530

    random emp check added

--- helloWorld.sh
+++ helloWorld.sh
@@ -1,1 +1,15 @@
+#!/bin/bash -x
+
 echo "Welcome to Employee Wage Calculation"
+
+isPresent=1;
+randomCheck=$((RANDOM%2));
+
+if [ $isPresent -eq $randomCheck ]
+then
+        empRatePerHr=20;
+        empHrs=8;
+        salary=$(($empHrs*$empRatePerHr));
+else
+        salary=0;
+fi
